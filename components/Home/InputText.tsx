6b3f456925ca6bfd1814c476d51e6b6e49c6d8ff
Harithya Wisesa <harithya77@gmail.com>
2023-04-07 06:52:21 +0700

    Membuat input pada card link

--- components/Home/InputText.tsx
+++ components/Home/InputText.tsx
@@ -1,14 +1,42 @@
 import { PencilIcon } from "@heroicons/react/24/outline";
-import React, { InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes, useRef, useEffect, useState } from "react";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
 }
 const InputText: React.FC<Props> = ({ className, ...props }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  useEffect(() => {
+    if (isFocus) {
+      inputRef.current?.focus();
+    }
+  }, [isFocus]);
+
   return (
-    <div className="flex space-x-6 items-center">
-      <input className={`w-full outline-none ${className}`} {...props} />
-      <PencilIcon className="h-5 w-5" />
+    <div className="flex space-x-3 items-center">
+      {isFocus ? (
+        <input
+          ref={inputRef}
+          onBlur={() => setIsFocus(false)}
+          className={`w-full min-w-fit outline-none ${className}`}
+          {...props}
+        />
+      ) : (
+        <React.Fragment>
+          <label className={`w-auto truncate ${className}`}>
+            {props.value}
+          </label>
+          <button
+            className="btn btn-circle btn-xs btn-ghost"
+            onClick={() => setIsFocus(true)}
+          >
+            <PencilIcon className="h-4 w-4" />
+          </button>
+        </React.Fragment>
+      )}
     </div>
   );
 };
