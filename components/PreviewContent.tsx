f43959727d0fd035f890d4dd07626fd48e4060dd
Harithya Wisesa <harithya77@gmail.com>
2023-04-08 20:24:21 +0700

    custom theme detail

--- components/PreviewContent.tsx
+++ components/PreviewContent.tsx
@@ -1,10 +1,57 @@
 import { ThemeContext } from "@/contexts/ThemeContext";
+import { ThemeContextType } from "@/types/contexts/theme-type";
 import Image from "next/image";
-import React from "react";
+import React, { useContext, useRef, useEffect } from "react";
 
 const PreviewContent = () => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const bgRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const { theme } = useContext<ThemeContextType>(ThemeContext);
+
+  const rgbToHex = (rgb: string) => {
+    const hex = Number(rgb.replace(/\D/g, "")).toString(16);
+    const res = hex.length === 1 ? `0${hex}` : hex;
+    return `#${res}`;
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log({
+        title: rgbToHex(
+          window
+            // @ts-ignore
+            .getComputedStyle(titleRef.current)
+            .getPropertyValue("color")
+        ),
+        bg: rgbToHex(
+          window
+            // @ts-ignore
+            .getComputedStyle(bgRef.current)
+            .getPropertyValue("background-color")
+        ),
+        button: rgbToHex(
+          window
+            // @ts-ignore
+            .getComputedStyle(buttonRef.current)
+            .getPropertyValue("background-color")
+        ),
+        textButton: rgbToHex(
+          window
+            // @ts-ignore
+            .getComputedStyle(buttonRef.current)
+            .getPropertyValue("color")
+        ),
+      });
+    }, 1000);
+  }, [theme.name]);
+
   return (
-    <div className="px-5 w-full h-screen bg-base-100 flex justify-center items-center">
+    <div
+      className="px-5 w-full h-screen bg-base-100 flex justify-center items-center"
+      ref={bgRef}
+    >
       <div className="flex justify-center flex-col items-center">
         <div className="avatar mb-5">
           <div className="w-20 mask mask-squircle">
@@ -16,12 +63,17 @@ const PreviewContent = () => {
             />
           </div>
         </div>
-        <h1 className="font-bold text-xl">@Haruthya</h1>
+        <h1 className="font-bold text-xl" ref={titleRef}>
+          @Haruthya
+        </h1>
         {/* <p className="text-gray-400 text-center text-xs px-10">
           Enjoy building everything from small app sites to elegant apps
         </p> */}
         <div className="w-full mt-10 space-y-5">
-          <button className="btn normal-case btn-primary w-full">
+          <button
+            className="btn normal-case btn-primary w-full"
+            ref={buttonRef}
+          >
             Twitter
           </button>
           <button className="btn normal-case btn-primary w-full">
--- pages/api/dummy.tsx
+++ pages/api/dummy.tsx
@@ -2,77 +2,152 @@ const listTheme = [
   {
     name: "Pastel",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871410/next-tree/photo_2023-04-07_19-42-53_bgrbf4.jpg",
-    primary: "#d1c1d7",
+    detail: {
+      title: "#7dc4f",
+      bg: "#f36e2d7",
+      button: "#c7808ff",
+      textButton: "#7f625",
+    },
   },
   {
     name: "Light",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871410/next-tree/photo_2023-04-07_19-42-51_cv4lof.jpg",
-    primary: "#570df8",
+    detail: {
+      title: "#4cb2b",
+      bg: "#f36e2d7",
+      button: "#84f420",
+      textButton: "#f36e2d7",
+    },
   },
   {
     name: "Cupcake",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871410/next-tree/photo_2023-04-07_19-42-54_kjf19l.jpg",
-    primary: "#65c3c8",
+    detail: {
+      title: "#64930",
+      bg: "#eea784d",
+      button: "#6081dc0",
+      textButton: "#1680",
+    },
   },
   {
     name: "Night",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871409/next-tree/photo_2023-04-07_19-42-55_fnk4cg.jpg",
-    primary: "#38bdf8",
+    detail: {
+      title: "#abd9b5f",
+      bg: "#25316",
+      button: "#3596140",
+      textButton: "#10a5",
+    },
   },
   {
     name: "Coffee",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680875027/next-tree/photo_2023-04-07_20-43-19_zc0rfm.jpg",
-    primary: "#DB924B",
+    detail: {
+      title: "#b2b27b",
+      bg: "#4eab7",
+      button: "#14e6433",
+      textButton: "#e79a",
+    },
   },
   {
     name: "Autumn",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871409/next-tree/photo_2023-04-07_19-42-58_yycsll.jpg",
-    primary: "#8C0327",
+    detail: {
+      title: "#b2b27b",
+      bg: "#4eab7",
+      button: "#14e6433",
+      textButton: "#e79a",
+    },
   },
   {
     name: "Luxury",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871410/next-tree/photo_2023-04-07_19-42-59_gnp8qx.jpg",
-    primary: "#ffffff",
+    detail: {
+      title: "#14ff240",
+      bg: "#26b7",
+      button: "#f36e2d7",
+      textButton: "#7dc4f",
+    },
   },
   {
     name: "Wireframe",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871409/next-tree/photo_2023-04-07_19-43-00_qm2ydg.jpg",
-    primary: "#b8b8b8",
+    detail: {
+      title: "#14ff240",
+      bg: "#26b7",
+      button: "#f36e2d7",
+      textButton: "#7dc4f",
+    },
   },
   {
     name: "Aqua",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871409/next-tree/photo_2023-04-07_19-43-01_cucioc.jpg",
-    primary: "#09ecf3",
+    detail: {
+      title: "#bc14fcf",
+      bg: "#50c46f",
+      button: "#8cef13",
+      textButton: "#20c1",
+    },
   },
   {
     name: "Lofi",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871411/next-tree/photo_2023-04-07_19-43-03_r70nfb.jpg",
-    primary: "#0D0D0D",
+    detail: {
+      title: "#00",
+      bg: "#f36e2d7",
+      button: "#200f1",
+      textButton: "#f36e2d7",
+    },
   },
   {
     name: "Valentine",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871410/next-tree/photo_2023-04-07_19-43-06_qq8uoe.jpg",
-    primary: "#e96d7b",
+    detail: {
+      title: "#f2c9b",
+      bg: "#e5160d8",
+      button: "#de4f683",
+      textButton: "#1a98",
+    },
   },
   {
     name: "Cyberpunk",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871411/next-tree/photo_2023-04-07_19-43-07_dkoivi.jpg",
-    primary: "#ff7598",
+    detail: {
+      title: "#c918",
+      bg: "#26f23c",
+      button: "#f34c760",
+      textButton: "#12123",
+    },
   },
   {
     name: "Synthwave",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871411/next-tree/photo_2023-04-07_19-43-09_dbxanj.jpg",
-    primary: "#e779c1",
+    detail: {
+      title: "#edb3615",
+      bg: "#451401",
+      button: "#dc6a129",
+      textButton: "#1119e",
+    },
   },
   {
     name: "Dark",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871410/next-tree/photo_2023-04-07_19-43-10_betcwo.jpg",
-    primary: "#1C4E80",
+    detail: {
+      title: "#9e79a03",
+      bg: "#67b9c",
+      button: "#9c0a36",
+      textButton: "#f36e2d7",
+    },
   },
   {
     name: "Corporate",
     img: "https://res.cloudinary.com/cv-abdi-creative/image/upload/v1680871410/next-tree/photo_2023-04-07_19-43-11_q1p1tg.jpg",
-    primary: "#4b6bfb",
+    detail: {
+      title: "#3b3d2",
+      bg: "#f36e2d7",
+      button: "#47a0bb3",
+      textButton: "#d01da67",
+    },
   },
 ];
 
