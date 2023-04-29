76bb571892df169a8ae7d19c546f2e1bbc3fbf3a
Harithya Wisesa <harithya77@gmail.com>
2023-04-29 20:29:30 +0700

    fix padding

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -9,7 +9,7 @@ const MainLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="w-full">
       <Header />
-      <div className="container-fluidpb-24">
+      <div className="container-fluid pb-24">
         <div className="">{children}</div>
       </div>
     </div>
